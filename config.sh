dcd359f907dad2a0c4e0c77c8378cab6d0987669
RAkushanchaminda <78252324+RAkushanchaminda@users.noreply.github.com>
2021-05-17 02:13:32 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,18 @@
 #! /bin/sh
 
-echo $(which apictl)
-apictl version
+echo $(which apimcli)
+apimcli version
 
-rm  -rf $HOME/.wso2apictl
+rm  -rf $HOME/.wso2apimcli
 
 echo 'setting up prod environment'
-apictl add env prod \
-                  --registration https://10.8.0.22:9444 \
-                  --publisher https://10.8.0.22:9444 \
-                  --devportal https://10.8.0.22:9444 \
-                  --admin https://10.8.0.22:9444 \
-                  --token https://10.8.0.22:8244/token \
-                  --mi https://10.8.0.22:9165
+apimcli add-env -n prod \
+                      --registration https://10.8.0.23:9444/client-registration/v0.14/register \
+                      --apim https://10.8.0.23:9444 \
+                      --token https://10.8.0.23:8244/token \
+                      --import-export https://10.8.0.23:9444/api-import-export-2.6.0-v1 \
+                      --admin https://10.8.0.23:9444/api/am/admin/v0.13 \
+                      --api_list https://10.8.0.23:9444/api/am/publisher/v0.14/apis \
+                      --app_list https://10.8.0.23:9444/api/am/store/v0.14/applications
 echo 'logging into prod'
 apictl login prod -u $USERNAME -p $PASSWORD
